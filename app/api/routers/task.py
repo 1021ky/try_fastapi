154f70b6e2b8ec79cf1cbf6d1b518aec254ef82e
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-10-11 12:37:17 +0000

    taskリソースを追加

---
+++ app/api/routers/task.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from app.api.schemas import task as task_schema
+
+router = APIRouter()
+
+@router.get("/tasks")
+async def list_tasks():
+    pass
+
+
+@router.post("/tasks")
+async def create_task():
+    pass
+
+@router.put("/tasks/{task_id}")
+async def update_task():
+    pass
+
+@router.delete("/tasks/{task_id}")
+async def delete_task():
+    pass
+
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
