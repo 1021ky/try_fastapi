15d733a27231b64ee2595dd04a5c92a72633d3f1
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-11-04 08:01:23 +0000

    isort

--- app/api/migrate_db.py
+++ app/api/migrate_db.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine
-
 from api.models.tasks import Base
+from sqlalchemy import create_engine
 
 DB_URL = "mysql+pymysql://root:developer@mysql:3306/demo?charset=utf8"
 engine = create_engine(DB_URL, echo=True)
